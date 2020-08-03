e9fe1ce7a5a830c49e7c13b216b3c03190ab4909
21shield <21netaly@gmail.com>
2020-08-02 22:28:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(arr, eventName) {
+    let thankYou = []
+    for (let i = 0; i< arr.length; i++){
+        thankYou.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYou  
+}
+
+function countDown(num) {
+    while(num>=0){
+        console.log(num--)
+    }
+}
